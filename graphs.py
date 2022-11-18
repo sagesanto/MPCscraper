f240651e613959adbc25ef7eed2840fbee00d008
Sage Santomenna <chellster1003@gmail.com>
2022-11-18 01:20:45 -0800

    graphs now. I NEED to look at data

--- graphs.py
+++ graphs.py
@@ -3,36 +3,68 @@ import pandas as pd
 from datetime import datetime
 
 graphs = []
-graphPath = "/graphs/" + str(datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M")) + '/'
+graphPath = "graphs/" + str(datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M")) + '/'
 os.mkdir(graphPath)
-os.mkdir(graphPath+"manual/") #idk if i can combine the previous line and this one
-
+os.mkdir(graphPath+"/barCharts")
 
 class GraphObj:
-    def __init__(self,name,dataframe,xaxis="Date",yaxis="Diagonal Residual"):
-        self.name, self.data, self.xAxis,self.yAxis= name,dataframe, xaxis,yaxis
+    global graphPath
+    def __init__(self,name,dataframe,xAxis="Date",yAxis="Diagonal Residual",path=graphPath):
+        self.name, self.dataframe, self.xAxis, self.yAxis, self.path = name, dataframe, xAxis, yAxis, path
+    def graph(self):
+        df = self.dataframe
+        tmoDf = (df.loc[(df["Obs Code"] == "654") | (df["Obs Code"] == 654)])
+        other = (df.loc[(df["Obs Code"] != "654") | (df["Obs Code"] != 654)])  # this sucks
+        plt.scatter(other[self.xAxis], other[self.yAxis], c='tab:blue')
+        plt.scatter(tmoDf[self.xAxis], tmoDf[self.yAxis], c='tab:orange')
+        plt.title(self.name)
+        plt.xlabel(self.xAxis)
+        plt.ylabel(self.yAxis)
+        plt.xticks([])
+        plt.savefig(self.path + self.name + ".png")
+        plt.clf()
+
 
 #read in the file
-df = pd.read_csv("data2.csv")
+data = pd.read_csv("data3.csv")
 
-current = df["2022" in df["Date"]]
-tmoCurrent = df[current["Obs Code"]==654]
+current = (data.loc[data['Date'].str.contains('2022')])
+tmoCurrent = (current.loc[(current["Obs Code"]=="654")])
 
-alltimeGraph = GraphObj("Alltime Observations",df)
+alltimeGraph = GraphObj("Alltime Observations",data)
 currentGraph = GraphObj("2022 Observations",current)
-alltimeTMOGraph = GraphObj("Alltime TMO", df[df["Obs Code"]==654])
-tmo2022Graph = GraphObj("TMO 2022", df[current["Obs Code"]==654])
-
+alltimeTMOGraph = GraphObj("Alltime TMO", (data.loc[(data["Obs Code"]=="654")]))
+tmo2022Graph = GraphObj("TMO 2022", (current.loc[(current["Obs Code"]=="654")]))
 
 #graph the manual graphs
-for graph in [alltimeGraph,currentGraph,alltimeTMOGraph,tmo2022Graph]:
-    tmoDf = df[graph.dataframe["Obs Code"]==654]
-    other = df[graph.dataframe["Obs Code"]!=654] #this sucks
-    plt.scatter(other[graph.xaxis], other[graph.yaxis], c='tab:blue')
-    plt.scatter(tmoDf[graph.xaxis],tmoDf[graph.yaxis],c='tab:orange')
-    plt.savefig(graphPath+"manual/"+graph.name+".png")
+graphs = [alltimeGraph, currentGraph, alltimeTMOGraph, tmo2022Graph]
 
-for file in os.list_dir("/obsCodesToPlot"):
+def plotMedians(codes,graphTitle):
+    global graphPath
+    medDf = pd.DataFrame(columns=["Obs Code", "Name", "Median Diagonal Error"])
+    for code in codes.keys():
+        df = current.loc[current["Obs Code"]==code]
+        lis = [code, codes[code],df["Diagonal Residual"].median()]
+        medDf.loc[len(medDf.index)] = lis
+    medDf = medDf.sort_values('Median Diagonal Error')
+    plt.bar(medDf['Name'],medDf["Median Diagonal Error"])
+    plt.title(graphTitle)
+    plt.ylabel("Median Diagonal Error (arcseconds)")
+    plt.xticks(fontsize=5, rotation=270)
+    plt.savefig(graphPath+"/barCharts/"+graphTitle+".png")
+    plt.clf()
+
+for file in os.listdir("obsCodesToPlot"):
     #read in line numbers, if Obs Code in [list]
-    graphs.append(GraphObj(file[:-4], ))
+    codes = {}
+    with open("obsCodesToPlot/"+file,'r') as f:
+        for line in f:
+            line = line.replace('\n','')
+            split = line.split(", ")
+            codes[split[0]] = split[1]
+    currentDf = current.loc[current["Obs Code"].isin(codes)]
+    plotMedians(codes,file[:-4])
+    graphs.append(GraphObj(file[:-4], currentDf))
 
+for graph in graphs:
+    graph.graph()
