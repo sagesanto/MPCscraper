c88d770553056a406ec410aaa3f4ad3ba82a8553
Sage Santomenna <chellster1003@gmail.com>
2022-11-20 00:34:45 -0800

    graphing done for now

--- graphs.py
+++ graphs.py
@@ -5,7 +5,8 @@ from datetime import datetime
 
 graphs = []
 graphPath = "graphs/" + str(datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M-%S")) + '/'
-
+#initialize colors
+# colors = [line for line in open("colors.txt")]
 
 class GraphObj:
     global graphPath
@@ -16,7 +17,6 @@ class GraphObj:
         codes = list(df["Obs Code"].unique())
         colorDict = createLegend(codes)
         fig, ax = plt.subplots()
-
         for code in codes:
             tempDf = df.loc[df["Obs Code"]==code]
             ax.scatter(tempDf[self.xAxis],tempDf[self.yAxis],c=colorDict[code],label=code)
@@ -30,41 +30,43 @@ class GraphObj:
         plt.clf()
 
 def createLegend(keys):
-    colors = mcolors.XKCD_COLORS
-    colors = list(colors.values())
-    random.shuffle(colors)
-    colors = colors[:len(keys)]
-    return {keys[i]: colors[i] for i in range(len(keys))}
+#for gradient
+    #global colors
+    # return {keys[i]: colors[i] for i in range(len(keys))}
+#for tmo only
+    colors = ["#0606A0"]*len(keys)
+    dict = {keys[i]: colors[i] for i in range(len(keys))}
+    dict["654"] = "#FFAB00"
+    return dict
 
 def plotMedians(codes,graphTitle):
     global graphPath
-    medDf = pd.DataFrame(columns=["Obs Code", "Name", "Median Diagonal Error"])
+    calcDf = pd.DataFrame(columns=["Obs Code", "Name", "Median Diagonal Error","Average Diagonal Error"])
+    
     for code in codes.keys():
         df = current.loc[current["Obs Code"]==code]
-        lis = [code, codes[code],df["Diagonal Residual"].median()]
-        medDf.loc[len(medDf.index)] = lis
-    medDf = medDf.sort_values('Median Diagonal Error')
-    if graphTitle == "Maunakea":
-        print(medDf)
-    fig,ax = plt.subplots()
-    codes = list(medDf["Obs Code"].unique())
-    colorDict = createLegend(codes)
-    fig, ax = plt.subplots()
-    for code in codes:
-        tempDf = medDf.loc[medDf["Obs Code"] == code]
-        bar = ax.bar(tempDf["Name"], tempDf["Median Diagonal Error"], color=colorDict[code])
-        ax.bar_label(bar, size=5, labels=['{:g}'.format(float('{:.2g}'.format(i))) for i in tempDf["Median Diagonal Error"]])
-    plt.title(graphTitle)
-    plt.ylabel("Median Diagonal Error (arcseconds)")
-    plt.xticks(fontsize=5, rotation=270)
-    x,y = medDf['Name'].values.tolist(), medDf["Median Diagonal Error"].values.tolist()
-
-    # for index in range(len(bar)):
-    #     ax.text(x[index], y[index], roundToN(y[index],2), size=4)
-    plt.savefig(graphPath+"/barCharts/"+graphTitle+".png")
-    plt.clf()
-
-#2018-A27 is an example of a repeat
+        lis = [code, codes[code],df["Diagonal Residual"].median(),df["Diagonal Residual"].mean()]
+        calcDf.loc[len(calcDf.index)] = lis
+    for column in ["Median Diagonal Error","Average Diagonal Error"]:
+        type = column.split(" ")[0]
+        calcDf = calcDf.sort_values(column)
+        fig,ax = plt.subplots()
+        codes = list(calcDf["Obs Code"].unique())
+        colorDict = createLegend(codes)
+        fig.set_size_inches(18.5, 10.5)
+        fig, ax = plt.subplots()
+        for code in codes:
+            tempDf = calcDf.loc[calcDf["Obs Code"] == code]
+            bar = ax.bar(tempDf["Name"], tempDf[column], color=colorDict[code],label=code)
+            ax.bar_label(bar, size=5, labels=['{:g}'.format(float('{:.2g}'.format(i))) for i in tempDf[column]])
+        plt.title(graphTitle+ ", by " + type + " Error")
+        plt.ylabel(column+" (arcseconds)")
+        plt.xticks(fontsize=10, rotation=270)
+        fig.subplots_adjust(bottom=0.3)
+        x,y = calcDf['Name'].values.tolist(), calcDf[column].values.tolist()
+        plt.savefig(graphPath+"/barCharts/"+graphTitle+type+".png", dpi=300)
+        plt.clf()
+        plt.close()
 
 #read in the file
 os.mkdir(graphPath)
@@ -74,16 +76,9 @@ os.mkdir(graphPath+"/barCharts")
 data = pd.read_csv("data3.csv")
 current = (data.loc[data['Date'].str.contains('2022')])
 tmoCurrent = (current.loc[(current["Obs Code"]=="654")])
-
-alltimeGraph = GraphObj("Alltime Observations",data)
-currentGraph = GraphObj("2022 Observations",current)
-alltimeTMOGraph = GraphObj("Alltime TMO", (data.loc[(data["Obs Code"]=="654")]))
-tmo2022Graph = GraphObj("TMO 2022", (current.loc[(current["Obs Code"]=="654")]))
-
-#graph the manual graphs
-graphs = [alltimeGraph, currentGraph, alltimeTMOGraph, tmo2022Graph]
-
-
+mauna = sum(current['Obs Code']=="568")
+maunaZeros = sum((current["Obs Code"]=="568")&(current["Diagonal Residual"]==0))
+print(maunaZeros, " of Maunakea's ",mauna," observations have a Diagonal Residual of zero.")
 
 for file in os.listdir("obsCodesToPlot"):
     #read in line numbers, if Obs Code in [list]
